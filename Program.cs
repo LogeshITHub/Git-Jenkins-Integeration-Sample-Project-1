8deaf47975125beba4e3b50d218f0d291198196d
LogeshITHub <logesh24it@gmail.com>
2026-01-24 00:25:57 +0530

    Changes 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿Console.WriteLine("Hello, Logesh! Welcome To Devops Env. This Your Practice Time");
+﻿Console.WriteLine("Hello, Logesh! Welcome To Devops Env." +
+    " This Your Practice Time : Final Success! My CI/CD is Live!");
  
